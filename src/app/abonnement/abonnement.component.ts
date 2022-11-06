84f4b7972a4050807e760bd608259dd7e36cd0fd
Djamal GANI <72838287+cprogrameur@users.noreply.github.com>
2022-11-06 10:50:56 +0100

    First commit

--- src/app/abonnement/abonnement.component.ts
+++ src/app/abonnement/abonnement.component.ts
@@ -3,7 +3,6 @@ import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms
 import { ActivatedRoute, Router } from '@angular/router';
 import { FileUpload } from '../models/file-upload.model';
 import { AuthService } from '../services/auth.service';
-import { FileUploadService } from '../services/file-upload.service';
 import { FirebaseService } from '../services/firebase.service';
 
 @Component({
@@ -14,6 +13,7 @@ import { FirebaseService } from '../services/firebase.service';
 export class AbonnementComponent implements OnInit {
   public ajouterBoisson!: UntypedFormGroup;
   public nomRestaurant!: string;
+  public type!: string;
   selectedFiles: FileList | undefined;
   currentFileUpload!: FileUpload;
   percentage!: number;
@@ -26,7 +26,6 @@ export class AbonnementComponent implements OnInit {
     private router: Router,
     private userService: AuthService,
     private firebaseService: FirebaseService,
-    private uploadService: FileUploadService
   ) {
     this.ajouterBoisson = this.fb.group({
       nom: ['', Validators.required],
@@ -41,29 +40,12 @@ export class AbonnementComponent implements OnInit {
     this.router.navigate(['/login'])
   }
   onSubmit() {
-    this.upload(this.ajouterBoisson.value)
   }
-  selectFile(event: any): void {
-    this.selectedFiles = event.target.files;
+  selecttype(type: any): void {
+    this.type = type;
   }
 
-  upload(datas: any): void {
-    const file = this.selectedFiles!.item(0);
-    this.selectedFiles = undefined;
-
-    this.currentFileUpload = new FileUpload(file!);
-    this.uploadService.pushFileToStorage(this.currentFileUpload, datas, 'boissons').subscribe(
-      percentage => {
-        this.percentage = Math.round(percentage!);
-      },
-      error => {
-        console.log(error);
-      }
-    );
-    this.ngOnInit()
-  }
   ngOnInit(): void {
-    this.nomRestaurant = this.route.snapshot.params['nomRestaurant'];
     this.firebaseService.getStatus(this.userService.getUid()!).subscribe(
       (res)=>{
         res.length==1 ? this.statut=true:this.statut=false
